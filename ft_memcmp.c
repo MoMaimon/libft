a20d5ac5352269280c21a56fbe5cf2cddb9d1554
Mohammad Abd Elhafiz <mabd-elh@c1r3s12.42amman.com>
2026-09-23 16:55:25 +0300

    ft_memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mabd-elh <mabd-elh@student.42amman.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/23 15:21:56 by mabd-elh          #+#    #+#             */
+/*   Updated: 2026/09/23 16:48:42 by mabd-elh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n && (((unsigned char *) s1)[i] || ((unsigned char *) s2)[i]))
+	{
+		if (((unsigned char *) s1)[i] != ((unsigned char *) s2)[i])
+			return (((unsigned char *) s1)[i] - ((unsigned char *) s2)[i]);
+		i++;
+	}
+	return (0);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mabd-elh <mabd-elh@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/23 12:16:43 by mabd-elh          #+#    #+#             */
-/*   Updated: 2026/09/23 14:59:44 by mabd-elh         ###   ########.fr       */
+/*   Updated: 2026/09/23 16:49:43 by mabd-elh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,5 @@ char	*ft_strchr(const char *s, int c);
 void	*ft_memchr(const void *s, int c, size_t n);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 #endif
